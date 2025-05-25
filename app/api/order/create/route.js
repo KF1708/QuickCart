debda841c7a7d0fa8bc4dccf0f34b278e13bce70
KF1708 <kftech.va@gmail.com>
2025-05-25 18:08:55 -0400

    Finished Project

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -17,7 +17,7 @@ export async function POST(request) {
 
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return acc + product.offerPrice * item.quantity;
+      return (await acc) + product.offerPrice * item.quantity;
     }, 0);
 
     await inngest.send({
@@ -33,7 +33,7 @@ export async function POST(request) {
 
     // clear user cart
 
-    const user = await User.findById(user);
+    const user = await User.findById(userId);
     user.cartItems = {};
     await user.save();
 
