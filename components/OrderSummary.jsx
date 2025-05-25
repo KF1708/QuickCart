a3be7843e7624067d62081b122748e3ec8bd5cb3
KF1708 <kftech.va@gmail.com>
2025-05-25 15:45:19 -0400

    Save

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -53,8 +53,38 @@ const OrderSummary = () => {
 
       let cartItemsArray = Object.keys(cartItems).map((key) => ({
         product: key,
+        quantity: cartItems[key],
       }));
-    } catch (error) {}
+
+      cartItemsArray = cartItemsArray.filter((item) => item.quantity > 0);
+
+      if (cartItemsArray.length === 0) {
+        return toast.error("Cart is empty");
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.post(
+        "/api/order/create",
+        {
+          address: selectedAddress._id,
+          items: cartItemsArray,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setCartItems({});
+        router.push("/order-placed");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
